9a9905567dc9c1631c64a117035cfb16ae19671f
wangxuan <wx2348115@sina.com>
2016-09-21 07:35:07 +0800

    first version

---
+++ src/main/java/sdww8591/App.java
@@ -0,0 +1,13 @@
+package sdww8591;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
